183e5f7b13a175a5ddf22de137c2afa7cf19b797
ReportHaori <90968781+ReportHaori@users.noreply.github.com>
2022-04-07 19:20:05 +0700

    Add files via upload

---
+++ thymeleaf-crud/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long empid;
+	
+	@Column(name = "name")
+	private String name;
+	@Column(name = "email")
+	private String email;
+	public long getEmpid() {
+		return empid;
+	}
+	public void setEmpid(long empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
